c555e466d3b13a08103c77cdb1cd1e2290f77cb1
mhrdwan <futuremhr@gmail.com>
2023-08-16 13:01:13 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data , setdata] = useState("");
+  const [datas , setdatas] = useState({});
+  const baseurl = 'https://incentivized-testnet.seinetwork.io/check-eligibility?seiAddress=';
+
+  const check = async () => {
+    try {
+      const response = await axios.get(`${baseurl}${data}`);
+      console.log(response.data);
+      setdatas(response.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  const getEligibleAmount = () => {
+    if (datas.eligibleAmount) {
+      let numbers = datas.eligibleAmount.replace(/\D/g, '');
+      return Math.floor(numbers / 1000000);
+    }
+    return "";
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input onChange={(e) => setdata(e.target.value)}></input>
+      <button onClick={check}>Check</button>
+      <p>Address<span><h5>{data}</h5></span></p>
+      <h2>eligibleAmount : {getEligibleAmount()} $SEI</h2>
+      <h2>reason : {datas.reason === undefined ? "Anda Beruntung" : datas.reason}</h2>
+    </>
   );
 }
 
