84c79e5e8464efbb23ae515f342988b1ae42c5ca
mhrdwan <futuremhr@gmail.com>
2023-08-16 17:43:10 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function App() {
-  const [data , setdata] = useState("");
-  const [datas , setdatas] = useState({});
+  const [data, setdata] = useState("");
+  const [datas, setdatas] = useState("");
+  const [alertgagal, setalertgagal] = useState("");
+  const [alertsukses, setalertsukses] = useState("");
+  const [Loading, setLoading] = useState(false)
   const baseurl = 'https://incentivized-testnet.seinetwork.io/check-eligibility?seiAddress=';
 
   const check = async () => {
     try {
+      setLoading(true)
       const response = await axios.get(`${baseurl}${data}`);
       console.log(response.data);
       setdatas(response.data);
+      setalertsukses("Api Aktif");
+      setalertgagal("");
+      setLoading(false)
     } catch (error) {
-      console.error("Error fetching data:", error);
+      setalertgagal("Api Dari Sei Sedang dimatikan / Down");
+      setalertsukses("");
+      setLoading(false)
     }
   };
 
@@ -25,13 +34,32 @@ function App() {
     return "";
   };
 
+  useEffect(() => {
+    check()
+  }, [data])
+
+  if (!alertgagal) {
+    return alertsukses
+  }
+
+
+
   return (
     <>
       <input onChange={(e) => setdata(e.target.value)}></input>
-      <button onClick={check}>Check</button>
-      <p>Address<span><h5>{data}</h5></span></p>
-      <h2>eligibleAmount : {getEligibleAmount()} $SEI</h2>
-      <h2>reason : {datas.reason === undefined ? "Anda Beruntung" : datas.reason}</h2>
+      <button onClick={()=>check()}>
+        {Loading ? 'Loading...' : 'Check'}
+      </button>
+      <p>Address Kamu : <span><h5>{data}</h5></span></p>
+      <h2>EigibleAmount : {getEligibleAmount()} $SEI</h2>
+      <h2>Reason : {datas.reason === undefined ? "" : datas.reason}</h2>
+      <h4>
+        <span style={{ color: 'black' }}>Status API : </span>
+        <span style={{ color: alertgagal ? 'red' : 'green' }}>
+          {alertgagal || alertsukses}
+        </span>
+      </h4>
+
     </>
   );
 }
